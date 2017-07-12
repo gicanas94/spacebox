72dd8d92951c031cc897ddccc53f886e5b21f082
Gabriel Ignacio Cañas <gicanas94@gmail.com>
2017-07-12 17:20:19 -0300

    Relaciones entre tablas en modelos.

--- app/Spacebox.php
+++ app/Spacebox.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Spacebox extends Model
 {
     protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function post()
+    {
+        return $this->hasMany('App\Post');
+    }
 }
